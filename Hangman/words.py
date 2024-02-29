097297cbd751610d836e9f99876f7d3e94447a7a
MFLR3 <138813219+MFLR3@users.noreply.github.com>
2024-02-29 15:05:23 +0000

    Create words.py

---
+++ Hangman/words.py
@@ -0,0 +1,12 @@
+words = [
+    "albatross", "ant", "anteater", "apple", "banana", "bat", "bear", "beaver", "carrot", "cat",
+    "caterpillar", "crocodile", "dolphin", "dog", "dromedary", "duck", "eagle", "elephant", "elephantine", "elephant seal",
+    "ferret", "falcon", "flamingo", "fox", "gazelle", "giraffe", "gorilla", "hamster", "hippopotamus", "hyena",
+    "ibex", "iguana", "jackal", "jackrabbit", "jaguar", "jellyfish", "kangaroo", "kite", "koala", "koala bear",
+    "lemming", "lemur", "lion", "llama", "mongoose", "monkey", "mouse", "narwhal", "newt", "numbat",
+    "ostrich", "ocelot", "octopus", "orangutan", "parrot", "panda", "penguin", "peacock", "quail", "quokka",
+    "rabbit", "rhinoceros", "reindeer", "seagull", "snake", "squirrel", "tapir", "tiger", "turtle", "toucan",
+    "umbrella", "uakari", "unicorn", "urchin", "viper", "vampire bat", "vulture", "vole", "walrus", "whale",
+    "wombat", "xerus", "x-ray fish", "xylophone", "xenon", "xenopus", "xenops", "yeti", "zebu", "zebra",
+    "zeppelin"
+]
