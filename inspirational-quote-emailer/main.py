5b79f582f5c050d3f15dc3afe0159b3c638bf642
MFLR3 <138813219+MFLR3@users.noreply.github.com>
2024-09-25 13:57:11 +0100

    Create main.py

---
+++ inspirational-quote-emailer/main.py
@@ -0,0 +1,27 @@
+import smtplib
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+SENDER_EMAIL = "placeholder"
+SENDER_PASSWORD = "placeholder"
+RECIPIENT_EMAIL = "placeholder"
+
+with open('quotes.txt', 'r') as file:
+    quotes = file.readlines()
+
+random_quote = random.choice(quotes)
+
+if weekday == 2:
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        # secure connection
+        connection.starttls()
+        connection.login(user=SENDER_EMAIL, password=SENDER_PASSWORD)
+        connection.sendmail(
+            from_addr=SENDER_EMAIL,
+            to_addrs=RECIPIENT_EMAIL,
+            msg=f"Subject:Hello\n\n{random_quote}"
+        )
